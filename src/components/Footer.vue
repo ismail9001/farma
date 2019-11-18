fb4443346c912d35a147b37c6b010a0159031617
Iskander <nizamov-iskander@yandex.ru>
2019-11-18 20:37:04 +0300

    -project init

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer>
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+</style>
