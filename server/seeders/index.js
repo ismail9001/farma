bb56cf6f3497ea350f516c4d15aa3c8e4ef8574e
Iskander <nizamov-iskander@yandex.ru>
2019-11-29 22:33:57 +0300

    - seed данных по надою

--- server/seeders/index.js
+++ server/seeders/index.js
@@ -1,13 +1,23 @@
-const {	sequelize, Feed } = require('../src/models/')
+const {
+	sequelize,
+	Feed,
+	Milk
+} = require('../src/models/')
 
 const Promise = require('bluebird')
 const feeds = require('./feeds.json')
+const milks = require('./milks.json')
 
 sequelize.sync({ force: true })
 	.then(async function () {
 		await Promise.all(
 			feeds.map(feed => {
 				Feed.create(feed)
+			})
+		)
+		await Promise.all(
+			milks.map(milk => {
+				Milk.create(milk)
 			})
 		)
 	})
